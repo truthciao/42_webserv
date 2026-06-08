c7abe6e152893c154d00c0b7682313a095580f59
Yali <yshi@f6r9s2.clusters.42paris.fr>
2026-06-08 18:35:56 +0200

    new branch

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+#include <cstdlib>
+
+int main()
+{
+	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd < 0) {
+		std::cerr << "Socket creation failed!" << std::endl;
+		return 1;
+	}
+
+	struct sockaddr_in address;
+	memset(&address, 0, sizeof(address));
+
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(8080);
+
+	if (bind(server_fd, (struct sockaddr * )&address, sizeof(address)) < 0)
+	{
+		std::cerr << "Bind failed! Port might be in use." << std::endl;
+		close(server_fd);
+		return (1);
+	}
+
+	if (listen(server_fd, 3) < 0)
+	{
+		std::cerr << "Listen Failed!" << std::endl;
+		close(server_fd);
+		return (1);
+	}
+
+	struct sockaddr_in cliend_address;
+	socklen_t client_addr_len = sizeof(cliend_address);
+
+	int	new_socket = accept(server_fd, (struct sockaddr *)&cliend_address, & client_addr_len);
+	if (new_socket < 0)
+	{
+		std::cerr << "Accept failed!" << std::endl;
+		close(server_fd);
+		return 1;
+	}
+	std::cout << "\n--- Connection established! ---\n" << std::endl;
+
+	char buffer[1024] = {0};
+	read(new_socket, buffer, 1024);
+
+	std::cout << "Browser sent the following HTTP Request:\n" << std::endl;
+	std::cout << buffer << std::endl;
+
+	const char *http_response =
+		"HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/html\r\n"
+        "Content-Length: 46\r\n"
+        "\r\n"
+        "<html><body><h1>Hello WebServ!</h1></body></html>";
+
+	write(new_socket, http_response, strlen(http_response));
+
+	close(new_socket);
+	close(server_fd);
+
+	return (0);
+}
